cd4884ef3901dd6ed314f1a15cd4cf2e433f8e6e
Kaushal Bhogale <2015kaushal.bhogale@ves.ac.in>
2017-11-10 12:15:14 +0530

    Add Facenet Code

---
+++ main/compareEYC.py
@@ -0,0 +1,54 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from scipy import misc
+import tensorflow as tf
+import numpy as np
+import sys
+import os
+import argparse
+import facenet
+import align.detect_face
+import csv
+import pandas as pd
+
+def main(args):
+
+    images, face_image_paths = facenet.load_and_align_data(args.image_files, args.image_size, args.margin, args.gpu_memory_fraction)
+    with tf.Graph().as_default():
+
+        with tf.Session() as sess:
+      
+            # Load the model
+            facenet.load_model(args.model)
+    
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+
+            # Run forward pass to calculate embeddings
+            feed_dict = { images_placeholder: images, phase_train_placeholder:False }
+            emb = sess.run(embeddings, feed_dict=feed_dict)
+
+            df = pd.DataFrame(emb,index=face_image_paths)
+            df.to_csv('embeddings.csv')
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('model', type=str, 
+        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
+    parser.add_argument('image_files', type=str, nargs='+', help='Images to compare')
+    parser.add_argument('--image_size', type=int,
+        help='Image size (height, width) in pixels.', default=160)
+    parser.add_argument('--margin', type=int,
+        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+    parser.add_argument('--gpu_memory_fraction', type=float,
+        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+    return parser.parse_args(argv)
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
